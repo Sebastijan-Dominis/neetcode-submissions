2a8c28b0e4bdca329dd8c6ef50adb7405467bb81
Sebastijan Dominis <154608156+Sebastijan-Dominis@users.noreply.github.com>
2026-07-23 14:05:15 +0200

    Add: sql-neetflex-banned-accounts - submission-2

---
+++ SQL/sql-neetflex-banned-accounts/submission-2.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT l1.account_id
+FROM log_info l1
+JOIN log_info l2
+ON l1.account_id = l2.account_id AND
+    l1.ip_address != l2.ip_address AND
+    l1.login <= l2.logout AND
+    l2.login <= l1.logout;
