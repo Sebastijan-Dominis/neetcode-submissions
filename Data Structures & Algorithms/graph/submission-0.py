4c865b28e689fbe06ca5263f06ca3e875d5b0c8f
Sebastijan Dominis <154608156+Sebastijan-Dominis@users.noreply.github.com>
2026-07-24 19:00:41 +0200

    Add: graph - submission-0

---
+++ Data Structures & Algorithms/graph/submission-0.py
@@ -0,0 +1,32 @@
+class Graph:
+    
+    def __init__(self):
+        self.adj = defaultdict(set)
+
+    def addEdge(self, src: int, dst: int) -> None:
+        self.adj[src].add(dst)
+
+    def removeEdge(self, src: int, dst: int) -> bool:
+        if src not in self.adj or dst not in self.adj[src]:
+            return False
+        self.adj[src].remove(dst)
+        return True
+
+    def hasPath(self, src: int, dst: int) -> bool:
+        visited = set()
+        return self.dfs(src, dst, visited)
+
+    def dfs(self, curr: int, dst: int, visited: set) -> bool:
+        if curr in visited:
+            return False
+        
+        if curr == dst:
+            return True
+
+        visited.add(curr)
+
+        for nei in self.adj[curr]:
+            if self.dfs(nei, dst, visited):
+                return True
+
+        return False
