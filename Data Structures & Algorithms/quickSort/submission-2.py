fd5bd43d22e56c1de6d8550d2d7ff1d990163ac6
Sebastijan Dominis <154608156+Sebastijan-Dominis@users.noreply.github.com>
2026-07-24 13:53:51 +0200

    Add: quickSort - submission-2

---
+++ Data Structures & Algorithms/quickSort/submission-2.py
@@ -0,0 +1,27 @@
+# Definition for a pair.
+# class Pair:
+#     def __init__(self, key: int, value: str):
+#         self.key = key
+#         self.value = value
+class Solution:
+    def quickSort(self, pairs: List[Pair]) -> List[Pair]:
+        self.quickSortHelper(pairs, 0, len(pairs) - 1)
+        return pairs
+    
+    def quickSortHelper(self, pairs, l, r):
+        if l >= r:
+            return pairs
+        
+        left = l
+        pivot = pairs[r]
+
+        for i in range(l, r):
+            if pairs[i].key < pivot.key:
+                pairs[left], pairs[i] = pairs[i], pairs[left]
+                left += 1
+        
+        pairs[r] = pairs[left]
+        pairs[left] = pivot
+
+        self.quickSortHelper(pairs, l, left-1)
+        self.quickSortHelper(pairs, left+1, r)
