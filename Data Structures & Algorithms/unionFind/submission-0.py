afaa2982a84f7eb22cc1a98dc49ead91438d4ccf
Sebastijan Dominis <154608156+Sebastijan-Dominis@users.noreply.github.com>
2026-07-25 15:59:13 +0200

    Add: unionFind - submission-0

---
+++ Data Structures & Algorithms/unionFind/submission-0.py
@@ -0,0 +1,33 @@
+class UnionFind:
+    
+    def __init__(self, n: int):
+        self.par = [i for i in range(n)]
+        self.rank = [1] * n
+        self.components = n
+
+    def find(self, x: int) -> int:
+        while x != self.par[x]:
+            self.par[x] = self.par[self.par[x]]
+            x = self.par[x]
+        return x
+
+    def isSameComponent(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+
+    def union(self, x: int, y: int) -> bool:
+        par_x, par_y = self.find(x), self.find(y)
+
+        if par_x == par_y:
+            return False
+        
+        if self.rank[par_x] > self.rank[par_y]:
+            self.par[par_y] = par_x
+            self.rank[par_y] += self.rank[par_x]
+        else:
+            self.par[par_x] = par_y
+            self.rank[par_x] += self.rank[par_y]
+        self.components -= 1
+        return True
+
+    def getNumComponents(self) -> int:
+        return self.components
