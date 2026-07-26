b7e2b724f78cc738844ed7c9847325c82188cf42
Sebastijan Dominis <154608156+Sebastijan-Dominis@users.noreply.github.com>
2026-07-26 23:51:27 +0200

    Add: kruskal - submission-2

---
+++ Data Structures & Algorithms/kruskal/submission-2.py
@@ -0,0 +1,39 @@
+class UnionFind:
+    def __init__(self, n: int):
+        self.par = [i for i in range(n)]
+        self.size = [1] * n
+
+    def find(self, x: int) -> int:
+        while x != self.par[x]:
+            self.par[x] = self.par[self.par[x]]
+            x = self.par[x]
+        return x
+
+    def union(self, n1: int, n2: int) -> bool:
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2:
+            return False
+        
+        if self.size[p1] >= self.size[p2]:
+            self.par[p2] = p1
+            self.size[p1] += self.size[p2]
+        else:
+            self.par[p1] = p2
+            self.size[p2] += self.size[p1]
+        return True
+
+class Solution:
+    def minimumSpanningTree(self, n: int, edges: List[List[int]]) -> int:
+        edges.sort(key = lambda x: x[2], reverse=True)
+        
+        unionFind = UnionFind(n)
+        components = n
+        res = 0
+
+        while edges and components > 1:
+            n1, n2, w = edges.pop()
+            if unionFind.union(n1, n2):
+                res += w
+                components -= 1
+        
+        return res if components == 1 else -1
