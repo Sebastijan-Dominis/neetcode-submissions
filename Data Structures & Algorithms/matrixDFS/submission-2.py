0be12a105b8d9513bda162017633d1ed2aef0cfd
Sebastijan Dominis <154608156+Sebastijan-Dominis@users.noreply.github.com>
2026-07-27 18:59:18 +0200

    Add: matrixDFS - submission-2

---
+++ Data Structures & Algorithms/matrixDFS/submission-2.py
@@ -0,0 +1,35 @@
+class Solution:
+    def countPaths(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        lr, lc = ROWS-1, COLS-1
+        
+        if grid[0][0] == 1 or grid[lr][lc] == 1:
+            return 0
+        
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        visit = set()
+
+        def dfs(r: int, c: int) -> int:
+            if r == lr and c == lc:
+                return 1
+            
+            count = 0
+            visit.add((r, c))
+
+            for dr, dc in directions:
+                nr, nc = r+dr, c+dc
+
+                if (
+                    min(nr, nc) < 0 or
+                    nr > lr or nc > lc or
+                    (nr, nc) in visit or
+                    grid[nr][nc] == 1
+                ):
+                    continue
+                
+                count += dfs(nr, nc)
+            
+            visit.remove((r, c))
+            return count
+        
+        return dfs(0, 0)
