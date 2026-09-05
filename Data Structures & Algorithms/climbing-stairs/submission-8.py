619ad54aa1073037b46e9628cfdea3c888856b74
Sebastijan Dominis <154608156+Sebastijan-Dominis@users.noreply.github.com>
2026-09-05 21:54:19 +0200

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1, 1
+
+        while n:
+            temp = one
+            one = one + two
+            two = temp
+            n -= 1
+        
+        return two
